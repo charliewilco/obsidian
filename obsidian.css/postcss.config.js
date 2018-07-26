c9bd8fbd64559f232e808aa304bb3cf0d0334077
Charles ⚡ <charlespeters42@gmail.com>
2018-07-26 11:21:30 -0700

    moving to yarn workspaces

---
+++ obsidian.css/postcss.config.js
@@ -0,0 +1,36 @@
+module.exports = ctx => ({
+  map: false,
+  plugins: {
+    "postcss-import": {
+      skipDuplicates: true
+    },
+    autoprefixer: {
+      remove: true
+    },
+    "postcss-custom-media": {},
+    "postcss-custom-properties": {
+      preserve: true,
+      appendVariables: true
+    },
+    "postcss-color-function": {},
+
+    // Analysis + Code Style
+    "postcss-export-vars": {
+      file: "css",
+      type: "json",
+      matches: []
+    },
+
+    "postcss-gzip": {},
+    "immutable-css": ctx.env !== "production" ? {} : false,
+
+    // Minifier
+    "css-mqpacker": {},
+    cssnano: ctx.env === "production" ? {} : false,
+
+    "postcss-reporter": {
+      plugins: ["postcss-gzip", "immutable-css", "postcss-mdgx"],
+      clearMessages: true
+    }
+  }
+});
