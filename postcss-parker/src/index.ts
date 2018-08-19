b3a84f4d0369508dbed855a297642698f0f1f3bc
Charles ⚡ <charlespeters42@gmail.com>
2018-08-19 00:51:26 -0700

    adding postcss plugin in typescript

--- postcss-parker/src/index.ts
+++ postcss-parker/src/index.ts
@@ -1,5 +1,6 @@
-const postcss = require('postcss');
-const fs = require('fs');
+
+import * as postcss from 'postcss'
+import * as fs from 'fs'
 const Parker = require('parker/lib/Parker');
 const metrics = require('parker/metrics/All');
 const format = require('json-format');
@@ -8,10 +9,10 @@ let defaultOptions = {
   outputFile: './parker.json'
 };
 
-module.exports = postcss.plugin('postcss-parker', options => {
+module.exports = postcss.plugin('postcss-parker', (options: any) => {
   let opts = Object.assign({}, defaultOptions, options);
 
-  return function(root, result) {
+  return function(root: postcss.Root, result: postcss.Result) {
     let reporter = new Parker(metrics);
 
     const { css } = root.toResult();
@@ -20,7 +21,7 @@ module.exports = postcss.plugin('postcss-parker', options => {
 
     let output = format(results, { type: 'space', size: 2 });
 
-    fs.writeFile(opts.outputFile, output, err => {
+    fs.writeFile(opts.outputFile, output, (err: Error) => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
