10f6cf6ef29dd5493c10a9bcfa0ec94c31c5b8ae
Charles ⚡ <charlespeters42@gmail.com>
2018-12-30 23:05:33 -0800

    updating next.config.js

--- documentation/next.config.js
+++ documentation/next.config.js
@@ -1,24 +1,44 @@
-const withTypescript = require('@zeit/next-typescript');
-const withMDX = require('@zeit/next-mdx')({
-  extension: /\.mdx?$/
-});
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW
+      ? require('next/constants')
+      : require('next-server/constants');
 
-const withCSS = require('@zeit/next-css');
+module.exports = (phase, { defaultConfig }) => {
+  const raw = {
+    test: /\.txt$/,
+    use: 'raw-loader'
+  };
 
-const raw = {
-  test: /\.txt$/,
-  use: 'raw-loader'
-};
-
-module.exports = withTypescript(
-  withCSS(
-    withMDX({
-      pageExtensions: ['js', 'jsx', 'mdx'],
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {
       webpack(config) {
         config.module.rules.push(raw);
 
         return config;
       }
-    })
-  )
-);
+    };
+  }
+  // ✅ Put the require call here.
+
+  const withCSS = require('@zeit/next-css');
+  const withTypescript = require('@zeit/next-typescript');
+  const withMDX = require('@zeit/next-mdx')({
+    extension: /\.mdx?$/
+  });
+
+  return withTypescript(
+    withCSS(
+      withMDX({
+        pageExtensions: ['js', 'jsx', 'mdx'],
+        webpack(config) {
+          config.module.rules.push(raw);
+
+          return config;
+        }
+      })
+    )
+  );
+};
